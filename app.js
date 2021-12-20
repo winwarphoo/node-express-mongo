6d331c3bed4895db7acccfd16d2ef682ac0b19d3
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2021-12-20 19:02:24 +0900

    login, signin機能

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const mongoose = require("mongoose");
 const bodyparser = require('body-parser');
 const fileUpload = require('express-fileupload');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const User = require('./schema/User');
 const Message = require('./schema/Message');
 
 const app = express();
@@ -10,6 +14,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(bodyparser());
 app.use("/image", express.static(path.join(__dirname, 'image')));
+app.use("/avatar", express.static(path.join(__dirname, 'avatar')));
+app.use(session({ secret: 'HogeFuga' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect('mongodb://root:password123@localhost:27017/chatapp?authSource=admin', function (err) {
@@ -27,9 +35,63 @@ app.get("/", function(req, res, next) {
   });
 });
 
+app.get("/signin", function(req, res, next) {
+  return res.render('signin')
+});
+
+app.post("/signin", fileUpload(), function(req, res, next) {
+  const avatar = req.files.avatar;
+  avatar.mv('./avatar/' + avatar.name, function(err) {
+    if (err) throw err;
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      avatar_path: '/avatar/' + avatar.name,
+    });
+    newUser.save((err) => {
+      if (err) throw err;
+      return res.redirect("/");
+    })
+  });
+});
+
+app.get("/login", function(req, res, next) {
+  return res.render('login')
+});
+
+app.post("/login", passport.authenticate('local', {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}));
+
+passport.use(new localStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) {return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (user.password != password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findOne({ _id: id }, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.get("/update", function(req, res, next) {
   return res.render('update');
 });
+
+
 
 app.post("/update", fileUpload(), function(req, res, next) {
   
